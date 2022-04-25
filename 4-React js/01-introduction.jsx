2da5c058068c8ec489bcab39881ef2b78e2f7797
OsamaAslam29 <osamaaslam029@gmail.com>
2022-04-25 23:16:15 +0500

    react first tut

---
+++ 4-React js/01-introduction.jsx
@@ -0,0 +1,38 @@
+
+            // Introduction to React JS + Installation 
+            
+//    React JS is a javascript library which is  developed by Facebook. 
+
+//    It helps in building highly engaging single-page web applications. React JS helps in breaking down complex UI into simpler components. React JS has a high demand in the field of Web development.
+
+//  Installation VS Code, Extensions and Node.js
+// Install VS code: You can Search ‘Download VS Code’ or directly click here to download VS Code. Open the setup and click next until the ‘Select Additional task’ Dialog box appears. Make sure to check all the options and click next to install the VS Code.
+
+// Install Node.js: You can Download Node.js by clicking here. Select your OS and click on next, and your Node.js will be installed.
+// Download react dev tools: Click here to add react dev tools chrome extension. It is a chrome extension, which shows your React component hierarchies.
+// After installation, open the windows power shell (You can do this by pressing Shift + right-click and selecting the windows PowerShell option inside file explorer) and write "node --version" and "npm --version" to check the installation and version of Node.js and Node Package manager (npm) on your computer.
+
+// Windows PowerShell
+// Copyright (C) Microsoft Corporation. All rights reserved.
+// Try the new cross-platform PowerShell https://aka.ms/pscore6
+// PS C:\Users\iitia> node --version
+// v16.13.0
+// PS C:\Users\iitia> npm --version
+// 8.1.0
+// PS C:\Users\iitia>
+// Getting Started with VS code:
+// Go to your desired folder and open it inside VS Code. You can do so by right-clicking followed by selecting the option of “Open with Code” in Windows.
+
+
+
+// Downloading  VS code extensions:
+// Thunderclient: It is a visual studio code extension that allows you to use 'GET' or 'POST’ requests and test your APIs by easily displaying the response.
+// ES7 React/redux/GraphQL/React-native snippets: It is just like an Emmet abbreviation, which allows you to autocomplete the code.
+// Bracket pair colorizer: It allows matching brackets so that they can be easily identified with colors.
+// Auto rename tag: Automatically rename paired HTML/XML tag.
+// Live server: It allows us to visualize our static page by opening the page with a live server.
+// In the upcoming lessons, we will be covering a lot of concepts of React with some amazing projects that would allow you to understand React completely. 
+
+// Thank you for being with me throughout the lesson. See you all in the next lesson where we will be creating our first react app!
+
+ 
