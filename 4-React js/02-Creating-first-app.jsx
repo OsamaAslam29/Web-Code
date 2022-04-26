ebc81fdfd68e5f91e2218d67cbb72318a490711e
OsamaAslam29 <osamaaslam029@gmail.com>
2022-04-26 18:35:51 +0500

    creating method of react app

--- 4-React js/02-Creating-first-app.jsx
+++ 4-React js/02-Creating-first-app.jsx
@@ -1,21 +1,22 @@
-// Creating our first react app using create-react-app | Complete React Course in Hindi #2
-// In the first tutorial, we installed the tools to start working with React. We will now understand the structure of a React app.
-
-// Component-based architecture of React:
-// In React, we create our app by dividing it into different components. We can create components of our app individually and can reuse them again and again. 
-
-// For Example: In the below Image, we have structured our app into the header, footer, body, and right navigation bar.
 
 
 
-// You can structure and design your react components as per your choice
+// Creating our first react app using create-react-app
 
-// Advantage of dividing our app into components:
-// By dividing your app into components, you can reuse the component in the same or different app repeatedly. 
-// We can change the component of our app using Props and State.
-// It also makes it easier to find errors in a large set of your code.
+
+//  Component-based architecture of React:
+//                                      In React, we create our app by dividing it into different components. We can create components of our app individually and can reuse them again and again. 
+
+
+// we can structure and design your react components as per your choice
+
+//                   Advantage of dividing our app into components:
+//  1: We can reuse the component in the same or different app repeatedly. 
+//  2: We can change the component of our app using Props and State.
+//  3: It also makes it easier to find errors in a large set of your code.
 
 // Let’s begin creating our app:
+
 // We can create React Application by writing the Code from Scratch, But it will only lead to wastage of our efforts, As we can Directly Use create-react-app Package.
 
 // npm: It stands for Node Package manager. It helps in Installing Packages in NodeJS.
@@ -27,6 +28,7 @@
 // Write the following command in Your VS Code Terminal. 
 
 // npx create-react-app textutils
+
 // Textutils is the name of our app. After running the command, a folder named textutils will be created.
 
 // Folder Structure of Textutils(Our React App):
@@ -42,11 +44,15 @@
 //  <div id="root"></div>
 // Here, we will be rendering data from components available in the src folder with the help of JavaScript.
 
-// "src" folder: Most of the time, we will be writing our code in the src folder. 
-// Two most important files in the src folder are:
+// "src" folder: 
+//              Most of the time, we will be writing our code in the src folder. 
 
-// index.js: This is the entry component of our app. It has the code of what to render and where.
-// app.js: With the help of Index.js, the content and changes in it are delivered to the mentioned element.
+//              Two most important files in the src folder are:
+
+// index.js:
+//       This is the entry component of our app. It has the code of what to render and where.
+// app.js:
+//       With the help of Index.js, the content and changes in it are delivered to the mentioned element.
 
 
 // How to run your React app?
