943e606d8b234a97c1ad799040090f2bb4887b14
OsamaAslam29 <osamaaslam029@gmail.com>
2022-04-27 17:36:35 +0500

    second topic clear

--- 4-React js/02-Creating-first-app.jsx
+++ 4-React js/02-Creating-first-app.jsx
@@ -32,14 +32,21 @@
 // Textutils is the name of our app. After running the command, a folder named textutils will be created.
 
 // Folder Structure of Textutils(Our React App):
-// node_modules: It contains all the packages, which are used by React app.
-// .gitignore: It contains the files, which you do not want to push in Github.
-// package.json: Contains all the packages which are installed in Node modules.
-// readme.md: It provides the basic info about your app.
-// The two folders in which we are interested are :
+// node_modules: 
+//              It contains all the packages, which are used by React app.
+// .gitignore:
+//              It contains the files, which you do not want to push in Github.
+// package.json:
+//              Contains all the packages which are installed in Node modules.
+// readme.md:
+ //             It provides the basic info about your app.
 
-// "public" folder: It contains an Index.html file.
-// index.html: It is the main HTML file of our react app. This is the page that is displayed on starting our application. It has an empty div tag like this:
+//                  The two folders in which we are interested are :
+
+// "public" folder:
+//                 It contains an Index.html file.
+// index.html: 
+//            It is the main HTML file of our react app. This is the page that is displayed on starting our application. It has an empty div tag like this:
 
 //  <div id="root"></div>
 // Here, we will be rendering data from components available in the src folder with the help of JavaScript.
@@ -55,13 +62,8 @@
 //       With the help of Index.js, the content and changes in it are delivered to the mentioned element.
 
 
-// How to run your React app?
+//                        =====> How to run your React app?
 // In index.js, open the terminal and write npm start, and your react app will be served at localhost:3000. You can make changes to the content of your React app in App.js. You can use npm run build if you are creating an app for production.
 
  
 
-// In the upcoming videos, we will be covering a lot of concepts of React with some amazing projects that would allow you to understand React completely. Do remember to Access the playlist of the Free React Course in Hindi by clicking here.
-
-// Check out our Github to access the source code at this point in the course,  Click Here
-
-// Thank you for being with me throughout the tutorial. In the next tutorial, we will recap JavaScript concepts which are needed in react. Till then, Keep learning and Keep Coding.
