a48ac17fb65a81c5d8dec4b081cfb69819ef3237
OsamaAslam29 <osamaaslam029@gmail.com>
2022-05-01 21:12:25 +0500

    new app

---
+++ 4-React js/textutils/module1.mjs
@@ -0,0 +1,8 @@
+ const a = "osama";
+ const a1 = "fraz";
+ const a2 = "umer";
+ const a3 = "aman";
+ const a4 = "nadeem";
+
+
+ export default a;
--- 4-React js/textutils/osama.mjs
+++
@@ -1,6 +0,0 @@
-import dz,{a,b,c,d} from './osama1.mjs'
- console.log(dz);
- console.log(a);
- console.log(b);
- console.log(c);
- console.log(d);
